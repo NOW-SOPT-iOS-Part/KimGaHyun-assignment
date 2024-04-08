ea78fafb33180d5e1fdcca92de8237d4d171540b
mcrkgus <mcrkgus@naver.com>
2024-04-08 14:46:23 +0900

    Asset 추가

--- TVING/TVING/Present/Login/View/LoginView.swift
+++ TVING/TVING/Present/Login/View/LoginView.swift
@@ -5,4 +5,65 @@
 //  Created by Gahyun Kim on 2024/04/08.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class LoginView: UIView {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    private let mainLabel = UILabel()
+    private let idTextField = UITextField()
+    private let passwordTextField = UITextField()
+    private let loginButton = UIButton()
+    private let idFindButton = UIButton()
+    private let passwordFindButton = UIButton()
+    private let nonAccountLabel = UILabel()
+    private let nicknameButton = UIButton()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setStyle()
+        setHierarchy()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+private extension LoginView {
+
+    func setStyle() {
+        mainLabel.do {
+            $0.text = StringLiterals.Login.mainTitle
+            $0.font = .pretendardFont(weight: 500, size: 23)
+            $0.textColor = .white
+            $0.textAlignment = .center
+        }
+        
+        idTextField.do {
+            $0.placeholder = StringLiterals.Login.idTextField
+        }
+    }
+    
+    func setHierarchy() {
+
+    }
+    
+    func setLayout() {
+
+    }
+}
--- TVING/TVING/Present/Login/ViewController/LoginViewController.swift
+++ TVING/TVING/Present/Login/ViewController/LoginViewController.swift
@@ -5,4 +5,41 @@
 //  Created by Gahyun Kim on 2024/04/08.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class LoginViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    private let loginView = LoginView()
+    
+    // MARK: - Life Cycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+// MARK: - Extensions
+
+private extension LoginViewController {
+
+    func setStyle() {
+        
+    }
+    
+    func setHierarchy() {
+
+    }
+    
+    func setLayout() {
+
+    }
+}
