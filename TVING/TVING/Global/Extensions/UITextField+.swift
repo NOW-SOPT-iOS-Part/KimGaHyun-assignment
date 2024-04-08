e7f84fcdae3740b0655395c6653265b98efd0d5b
mcrkgus <mcrkgus@naver.com>
2024-04-08 15:23:10 +0900

    Extension 추가

---
+++ TVING/TVING/Global/Extensions/UITextField+.swift
@@ -0,0 +1,30 @@
+//
+//  UITextField+.swift
+//  TVING
+//
+//  Created by Gahyun Kim on 2024/04/08.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    /// 텍스트필드 안쪽에 패딩 추가
+    /// - Parameter left: 왼쪽에 추가할 패딩 너비
+    /// - Parameter right: 오른쪽에 추가할 패딩 너비
+    func addPadding(left: CGFloat? = nil, right: CGFloat? = nil) {
+        if let left {
+            leftView = UIView(frame: CGRect(x: 0, y: 0, width: left, height: 0))
+            leftViewMode = .always
+        }
+        if let right {
+            rightView = UIView(frame: CGRect(x: 0, y: 0, width: right, height: 0))
+            rightViewMode = .always
+        }
+    }
+    
+    /// Placeholder의 색상을 바꿔주는 메서드
+    func changePlaceholderColor(forPlaceHolder: String, forColor: UIColor) {
+        self.attributedPlaceholder = NSAttributedString(string: forPlaceHolder, attributes: [NSAttributedString.Key.foregroundColor: forColor])
+    }
+}
