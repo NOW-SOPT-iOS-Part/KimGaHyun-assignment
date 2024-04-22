ab393dfc8374ceca77915ce82520d47a5a8c3f1e
mcrkgus <mcrkgus@naver.com>
2024-04-22 15:29:15 +0900

    [Feat] #WEEK3 - HomeModel Dummy Data 추가

---
+++ TVING/TVING/Present/Home/Model/HomeModel.swift
@@ -0,0 +1,30 @@
+//
+//  HomeModel.swift
+//  TVING
+//
+//  Created by Gahyun Kim on 2024/04/22.
+//
+
+import UIKit
+
+struct HomeModel {
+    let movieList: [MovieList]
+}
+
+
+struct MovieList {
+    let movieImg: UIImage
+    let movieTitle: String
+}
+
+extension MovieList {
+    static func dummy() -> [MovieList] {
+        return [
+            MovieList(movieImg: .imgMovie1, movieTitle: "시그널"),
+            MovieList(movieImg: .imgMovie2, movieTitle: "해리포터와 마법사의 돌"),
+            MovieList(movieImg: .imgMovie3, movieTitle: "반지의 제왕"),
+            MovieList(movieImg: .imgMovie4, movieTitle: "스즈메의 문단속"),
+            MovieList(movieImg: .imgMovie5, movieTitle: "너의 이름은")
+        ]
+    }
+}
