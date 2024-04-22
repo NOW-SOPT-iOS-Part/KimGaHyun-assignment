dba937b68a06ad051a4d07205088fa259c39f756
mcrkgus <mcrkgus@naver.com>
2024-04-22 15:09:35 +0900

    [Feat] #WEEK3 - Home View VC 분리

--- TVING/TVING/Present/Home/View/HomeView.swift
+++ TVING/TVING/Present/Home/View/HomeView.swift
@@ -5,4 +5,48 @@
 //  Created by Gahyun Kim on 2024/04/22.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeView: UIView {
+    
+    // MARK: - Properties
+    
+    private let collectionViewLayout = CompositionalFactory.create()
+    private(set) lazy var collectionView = UICollectionView(frame: .zero,
+                                                            collectionViewLayout: collectionViewLayout)
+
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+private extension HomeView {
+    // MARK: - set Hierarchy
+    
+    func setupHierarchy() {
+        self.addSubview(collectionView)
+    }
+    
+    // MARK: - set Layout
+    
+     func setupLayout() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
--- TVING/TVING/Present/Home/ViewController/HomeViewController.swift
+++ TVING/TVING/Present/Home/ViewController/HomeViewController.swift
@@ -5,4 +5,79 @@
 //  Created by Gahyun Kim on 2024/04/22.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    private let homeView = HomeView()
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        self.view = homeView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setAddTarget()
+        setDelegate()
+        setRegister()
+    }
+}
+
+
+private extension HomeViewController {
+    func setAddTarget() {
+        
+    }
+    
+    func setDelegate() {
+        
+    }
+    
+    func setRegister() {
+        
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 5
+        case 2:
+            return 5
+        case 3:
+            return 5
+        default:
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+
