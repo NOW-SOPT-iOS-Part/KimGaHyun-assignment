90e6b74f2023ab28db73a1053d385f47165f2dd7
mcrkgus <mcrkgus@naver.com>
2024-06-03 16:28:09 +0900

    [Add] #7 - MovieDTO 추가

--- TVING/TVING/Present/Movie/Model/MovieDTO.swift
+++ TVING/TVING/Present/Movie/Model/MovieDTO.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct MovieDTO: Codable {
+    var rank: String
+    var movieNm: String
+    var openDt: String
+    var audiCnt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case rank, movieNm, openDt
+        case audiCnt = "audiAcc"
+    }
+}
