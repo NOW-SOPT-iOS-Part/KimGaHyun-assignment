fbd469adae3c0ce2d31e920a89f115b6ac3f3b70
mcrkgus <mcrkgus@naver.com>
2024-06-03 16:26:25 +0900

    [Refact] #7 - MVVM 패턴 적용

---
+++ TVING/TVING/Present/Movie/Model/MovieDTO.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDTO.swift
+//  TVING
+//
+//  Created by Gahyun Kim on 2024/06/03.
+//
+
+import Foundation
---
+++ TVING/TVING/Present/Movie/ViewModel/MovieViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieViewModel.swift
+//  TVING
+//
+//  Created by Gahyun Kim on 2024/06/03.
+//
+
+import Foundation
